bca29b15fe485feb178e53ec790609ac4c65737c
Aghiles <Aghilesham20@gmail.com>
2021-08-29 16:41:02 -0500

    Third Push:Definition,Mathprob

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -7,28 +7,27 @@ public class Factorial {
 
      Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120
      */
+    public static int Recursion ( int num){
+        if (num == 0 || num == 1)
+            return 1;
+        else
+            return (num * Recursion(num - 1));
+    }
 
+    public static int Iteration ( int n){
+
+        if (n == 0)
+            return 1;
+        int i = n, fact = 1;
+        while (n / i != n) {
+            fact = fact * i;
+            i--;
+        }
+        return fact;
+    }
     public static void main(String[] args) {
-
-        public static int Recursion ( int num){
-            if (num == 0 || num == 1)
-                return 1;
-            else
-                return (num * Recursion(num - 1));
-        }
-
-        public static int Iteration ( int n){
-
-            if (n == 0)
-                return 1;
-            int i = n, fact = 1;
-            while (n / i != n) {
-                fact = fact * i;
-                i--;
-            }
-            return fact;
-        }
-        {
+        Iteration(5);
+        Recursion(10);
 
 
         }
